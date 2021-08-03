52f0eb0ca00bd257a22914b62399a2e17c15cdcd
Christine Dewi <46774451+ChristineDewi@users.noreply.github.com>
2021-08-03 13:36:58 +0800

    Update detect_blinks_2.py

--- detect_blinks_2.py
+++ detect_blinks_2.py
@@ -1,4 +1,4 @@
-# USAGE
+# USAGE CHRISTINE 2021
 # python detect_blinks.py --shape-predictor shape_predictor_68_face_landmarks.dat --video blink_detection_demo.mp4
 # python detect_blinks.py --shape-predictor shape_predictor_68_face_landmarks.dat
 
